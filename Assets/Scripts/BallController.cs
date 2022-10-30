6d9d1ed0ca6105d4d25292c1d0adcc595694cb5e
Sedat Doğan <dogannnsedat@gmail.com>
2022-10-30 18:44:12 +0300

    1.Level tasarlandı

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     Rigidbody rb;
-    float pushForce = 500f;
-    public float horizontalMoveSpeed = 2f;
+    float pushForce = 200f;
+    public float horizontalMoveSpeed;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,11 +15,12 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontalMove = Input.GetAxis("Horizontal");//Yatay hareket girdisi    
-        rb.AddForce(0, 0, pushForce * Time.fixedDeltaTime);//Pozisyonuna güç uygular
-        rb.velocity = new Vector3(horizontalMove*horizontalMoveSpeed, rb.velocity.y,rb.velocity.z);
-       
-       
+        float horizontalMove = Input.GetAxis("Horizontal");//Yatay hareket girdisi
+        float verticalMove = Input.GetAxis("Vertical");
+        //transform.Translate(new Vector3(0, 0, verticalMove*horizontalMoveSpeed* Time.fixedDeltaTime));
+        rb.AddForce(0, 0, pushForce * Time.fixedDeltaTime);//Pozisyonuna güç uygular.Ýleri itme
+        rb.velocity = new Vector3(horizontalMove*horizontalMoveSpeed, rb.velocity.y,rb.velocity.z);//Saða sola hareket
+        //rb.AddTorque(0, 0, -(horizontalMove * horizontalMoveSpeed));
         //rb.AddTorque(1, 0, 0, ForceMode.Impulse);//Rotasyonuna güç uygular
     }
 }
